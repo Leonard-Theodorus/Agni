eb1ca7fad6e3ac4f578d79d777edb9a556104203
Leonard Theodorus <aoesotfs@gmail.com>
2023-10-07 14:14:00 +0700

    feat [first commit]

---
+++ Igni/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Igni
+//
+//  Created by Leee on 07/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Igni/IgniApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgniApp.swift
+//  Igni
+//
+//  Created by Leee on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IgniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
